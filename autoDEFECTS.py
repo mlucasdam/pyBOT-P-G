5e0fce583b68b2c1b93b8e04f51802a5149f5245
Damiani <damiani.lm@pg.com>
2022-10-29 19:34:53 -0300

    primeiro comit

---
+++ autoDEFECTS.py
@@ -0,0 +1,31 @@
+import pyautogui as py
+import time
+
+defects = 26
+i = 0
+while i < defects:
+    py.alert('runing script...')
+    py.click(x=1160,y=92)
+    time.sleep(1)
+    py.click(x=289,y=321)
+    py.write('Bandeja de nylon estava suja')
+
+    py.click(x=296,y=567)
+    py.click(x=282,y=655)
+    py.click(x=278,y=623)
+    py.click(x=278,y=657)
+    py.click(x=253,y=666)
+    py.click(x=266,y=713)
+    py.click(x=266,y=715)
+    py.click(x=265,y=756)
+    py.click(x=264,y=766)
+    py.click(x=280,y=808)
+
+    time.sleep(1)
+    py.click(x=1144,y=79)
+    py.click(x=1202,y=95)
+    time.sleep(5)
+
+    if i == defects:
+        break
+    i+=1
