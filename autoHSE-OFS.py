43e31a09189c2877044a9b89571503641a33735d
Damiani <damiani.lm@pg.com>
2022-10-31 21:12:27 -0300

    incrementando o codigo

--- autoHSE-OFS.py
+++ autoHSE-OFS.py
@@ -2,7 +2,7 @@ from email.policy import default
 import pyautogui as py
 import time
 
-OFS = 4
+OFS = 2
 i = 0
 
 py.alert('runing script...')
@@ -33,38 +33,71 @@ while i < OFS:
     time.sleep(1)
     py.click(x=529,y=738)
     time.sleep(2)
-    py.click(x=598,y=796)
-    time.sleep(1)
-    py.click(x=519,y=838)
-    time.sleep(2)
-
-    #Preenchimento do comportamento e do comentario
-    py.click(x=879, y=900)
-    time.sleep(1)
-
-    behavior = py.prompt(title='escolha um dos 3', text='1- óculos de proteção, 2- colete refletivo, 3- protetor auricular', default = '1')
-
-    if behavior == '1':
-        py.click(x=513, y=344)
+    
+    #escolhendo o tipo de ofs
+    ofsType = py.prompt(title='escolha um dos 2', text='1- Reforçado, 2- Em risco', default = '1')
+    
+    if ofsType == '1':
+        py.click(x=598,y=796)
         time.sleep(1)
-        py.write('oculos')
-        py.click(x=578, y=598)
-        py.write('colaborador fez o uso correto dos oculos de protecao nas areas exigidas')
+        py.click(x=519,y=838)
         time.sleep(2)
-    elif behavior == '2':
-        py.click(x=513, y=344)
+        py.click(x=879, y=900)
         time.sleep(1)
-        py.write('colete refletivo')
-        py.click(x=578, y=598)
-        py.write('colaborador fez o uso correto do colete refletivo nas areas de transito misto')
-        time.sleep(2)
-    elif behavior == '3':
-        py.click(x=513, y=344)
+        behavior = py.prompt(title='escolha um dos 3', text='1- óculos de proteção, 2- colete refletivo, 3- protetor auricular', default = '1')
+
+        if behavior == '1':
+            py.click(x=513, y=344)
+            time.sleep(1)
+            py.write('oculos')
+            py.click(x=578, y=598)
+            py.write('colaborador fez o uso correto dos oculos de protecao nas areas exigidas')
+            time.sleep(2)
+        elif behavior == '2':
+            py.click(x=513, y=344)
+            time.sleep(1)
+            py.write('colete refletivo')
+            py.click(x=578, y=598)
+            py.write('colaborador fez o uso correto do colete refletivo nas areas de transito misto')
+            time.sleep(2)
+        elif behavior == '3':
+            py.click(x=513, y=344)
+            time.sleep(1)
+            py.write('protetor auricular')
+            py.click(x=578, y=598)
+            py.write('colaborador fez o uso correto dos protetores auriculares nas areas exigidas')
+            time.sleep(2)
+
+    elif ofsType == '2':
+        py.click(x=598,y=796)
         time.sleep(1)
-        py.write('protetor auricular')
-        py.click(x=578, y=598)
-        py.write('colaborador fez o uso correto dos protetores auriculares nas areas exigidas')
+        py.click(x=525,y=894)
         time.sleep(2)
+        py.click(x=879, y=900)
+        time.sleep(1)
+        behavior = py.prompt(title='escolha um dos 3', text='1- óculos de proteção, 2- colete refletivo, 3- protetor auricular', default = '1')
+
+        if behavior == '1':
+            py.click(x=513, y=344)
+            time.sleep(1)
+            py.write('oculos')
+            py.click(x=578, y=598)
+            py.write('colaborador nao estava de oculos quando entrou na area, foi avisado por seus colegas e os colocou')
+            time.sleep(2)
+        elif behavior == '2':
+            py.click(x=513, y=344)
+            time.sleep(1)
+            py.write('colete refletivo')
+            py.click(x=578, y=598)
+            py.write('colaborador nao estava com o colete regletivo nas areas de transito misto, foi avisado por seus colegas e o colocou')
+            time.sleep(2)
+        elif behavior == '3':
+            py.click(x=513, y=344)
+            time.sleep(1)
+            py.write('protetor auricular')
+            py.click(x=578, y=598)
+            py.write('colaborador nao estava com os protetores auriculares quando entrou na area, foi avisado por seus colegas e os colocou')
+            time.sleep(2)
     
     py.click(x=629, y=954)
     time.sleep(1)
